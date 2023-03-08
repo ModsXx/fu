cdce576294b309bbb2efbdf74cfcab277658d7de
ModsXx <118561210+ModsXx@users.noreply.github.com>
2023-03-08 04:37:03 -0500

    Add files via upload

---
+++ assets/preload/stages/white.lua
@@ -0,0 +1,20 @@
+function onCreate()
+	makeLuaSprite('white', 'white', -400,-700);
+	addLuaSprite('white', false);
+	scaleLuaSprite('white', 80.0, 80.0);
+
+
+end
+
+
+function onBeatHit()-- for every beat
+	-- body
+end
+
+function onStepHit()-- for every step
+	-- body
+end
+
+function onUpdate()
+	-- body
+end
